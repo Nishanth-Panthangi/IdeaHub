61d216d221e17b5ef3d83d6737718cb85a3d92ce
Nishanth Panthangi <nishanthkwel@gmail.com>
2023-08-11 12:33:46 -0500

    Spring Set Up initial commit

---
+++ src/main/java/com/ideahub/IdeaHub/IdeaHubApplication.java
@@ -0,0 +1,13 @@
+package com.ideahub.IdeaHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdeaHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdeaHubApplication.class, args);
+	}
+
+}
