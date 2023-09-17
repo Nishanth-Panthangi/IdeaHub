7c5ad238e8097a7396d1ab8a0bc58487841be824
Nishanth Panthangi <nishanthkwel@gmail.com>
2023-09-16 20:21:57 -0500

    Added getter setter to User model

--- src/main/java/com/ideaXhub/models/User.java
+++ src/main/java/com/ideaXhub/models/User.java
@@ -3,25 +3,6 @@ package com.ideaXhub.models;
 import javax.persistence.*;
 import java.util.Date;
 
-
-/**
- * User Class
- * id
- * email
- * password
- * firstName
- * lastName
- */
-
-/**
- * User Class
- * id
- * email
- * password
- * firstName
- * lastName
- */
-
 @Entity
 @Table(name = "users")
 public class User {
@@ -49,5 +30,84 @@ public class User {
     private boolean isActive;
     private boolean emailVerified;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public Date getLastLogin() {
+        return lastLogin;
+    }
+
+    public void setLastLogin(Date lastLogin) {
+        this.lastLogin = lastLogin;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void setActive(boolean active) {
+        isActive = active;
+    }
+
+    public boolean isEmailVerified() {
+        return emailVerified;
+    }
+
+    public void setEmailVerified(boolean emailVerified) {
+        this.emailVerified = emailVerified;
+    }
 }
 
