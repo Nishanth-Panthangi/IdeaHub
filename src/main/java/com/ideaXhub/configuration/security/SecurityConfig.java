b0dbdb2f2394cab84d0dee4769efd2d9c44c1c79
Nishanth Panthangi <nishanthkwel@gmail.com>
2024-02-14 19:07:28 -0600

    Played with security config

--- src/main/java/com/ideaXhub/configuration/security/SecurityConfig.java
+++ src/main/java/com/ideaXhub/configuration/security/SecurityConfig.java
@@ -1,7 +1,30 @@
 package com.ideaXhub.configuration.security;
 
-import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
-@Configuration
-public class SecurityConfig {
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        // Set my config on the auth object
+        auth.inMemoryAuthentication()
+                .withUser("nishanth")
+                .password("ideaxhub")
+                .roles("ADMIN")
+                .and()
+                .withUser("user")
+                .password("password")
+                .roles("USER");
+    }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
+    }
+
 }
